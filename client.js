6e68155bc85a203f62c0f0535733da5772ce4aea
Terry Ho <101287576+chung900811@users.noreply.github.com>
2022-08-10 01:00:45 -0700

    added use setTimeout to move up

--- client.js
+++ client.js
@@ -1,23 +1,32 @@
 const net = require("net");
-const { IP, PORT } = require("./constants");
 
-const connect = function() {
+// establishes a connection with the game server
+const connect = function () {
   const conn = net.createConnection({
-    host: IP,
-    port: PORT
+    host: "localhost",
+    port: 50541
   });
-  conn.setEncoding("utf8");
+  
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
 
-  conn.on("data", data => {
-    console.log(data);
-  });
-  conn.on("connect", () => {
+  conn.on("connect",() => {
     console.log("Successfully connected to game server");
   });
+  
   conn.on("connect", () => {
-    conn.write("Name: TERRY");
-  });
+    conn.write("Name: TH")
+  })
+
+  conn.on("connect", () => {
+    setTimeout(()=>{conn.write("Move: up")}, 3000)
+  })
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
   return conn;
 };
 
-module.exports = connect;
+module.exports = (connect);
