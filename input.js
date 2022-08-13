211f15069379670f121b2ab47001fe9dcd830774
Terry Ho <101287576+chung900811@users.noreply.github.com>
2022-08-13 01:01:11 -0700

    updated input.js

--- input.js
+++ input.js
@@ -1,39 +1,36 @@
-let connection;
+const connect = require("./client");
+let connection = connect();
 
-const setupInput = function(conn) {
-  connection = conn;
+const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
-
   return stdin;
 };
 
-const handleUserInput = key => {
-  if (key === "\u0003") {
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
     process.exit();
   }
-  if (key === "w") {
+  if (key === 'w') {
     connection.write("Move: up");
   }
-  if (key === "s") {
-    connection.write("Move: down");
-  }
-  if (key === "a") {
+  if (key === 'a') {
     connection.write("Move: left");
   }
-
-  if (key === "d") {
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
     connection.write("Move: right");
   }
-  if (key === "v") {
-    connection.write("Say: NICE");
-  }
-  if (key === "t") {
-    connection.write("Say: HELLO");
+  if (key === 'q') {
+    connection.write("Say: Hello!");
   }
 };
 
-module.exports = { setupInput };
+
+
+module.exports = (setupInput);
